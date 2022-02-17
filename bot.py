1b9dbfa57670f2f99f29ad2f478adbe31ad9c50f
pwpw0517 <99656147+pwpw0517@users.noreply.github.com>
2022-02-17 21:23:33 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,10 +1,46 @@
 import discord
 from discord.ext import commands
+import json
 
-bot = commands.Bot(command_prefix='!')
+with open('setting.json','r',encoding='utf8')as jfile:
+    jdata = json.load(jfile)
+
+
+bot = commands.Bot(command_prefix='')
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send(f'{round(bot.latency*1000)}(ms)')
+
+@bot.command()
+async def kevin好帥(ctx):
+    await ctx.send('真的挺帥的')
+
+@bot.command()
+async def 是吧(ctx):
+    await ctx.send('是阿')
+
+@bot.command()
+async def 對吧(ctx):
+    await ctx.send('對阿')
+
+@bot.command()
+async def 羊又騎請我吃早餐(ctx):
+    await ctx.send('羊又騎請他吃早餐')
+
+
+@bot.command()
+async def 欠錢還錢(ctx):
+    await ctx.send('曾士恩欠我200')
+
+@bot.command()
+async def 你出(ctx):
+    await ctx.send('曾士恩出')
+
+
 
 @bot.event
 async def on_ready():
     print('>>Bot is online<<')
 
-bot.run('OTQyNzQ2NDE2NjU1MjY1ODEz.Ygo_BQ.Ix-SYfyl_i0nymfmNZcgYUPyoPQ')
+bot.run(jdata['TOKEN'])
